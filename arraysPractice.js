4e18ca5de3c745fa84c40e1fb43b65fa77f56364
Zander Allen <ztrain01@gmail.com>
2015-01-22 20:26:46 -0700

    Second problem completed

--- arraysPractice.js
+++ arraysPractice.js
@@ -23,9 +23,15 @@ var arr = [40,50,60];
 //Create a function named 'last' that is given 'arr' as the argument and returns the last item the given array.
 
 
-  //Code Here
+var last = function() {
 
+  return function() {
+    return arr[2];
+  }
+}
 
+var bar = last();
+console.log(bar());
 //Next Problem
 
 
